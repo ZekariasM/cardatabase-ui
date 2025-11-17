d8111d36237159bfc7889367f314a505722ddf62
zekariasm <zekariasmulu@gmail.com>
2025-11-17 00:05:20 -0800

    Implementing fetching data from the backend

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
+import {QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Carlist from './components/Carlist';
+
+const queryClient = new QueryClient();
 
 export default function App() {
 
@@ -17,6 +21,9 @@ export default function App() {
           </Typography>
         </Toolbar>
       </AppBar>
+      <QueryClientProvider client={queryClient}>
+        <Carlist />
+      </QueryClientProvider>
     </Container>
     </>
   )
---
+++ src/components/Carlist.tsx
@@ -0,0 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
+import type { CarResponse } from "../types"
+import axios from "axios"
+
+export default function Carlist() {
+    const getCars = async(): Promise<CarResponse[]> => {
+        const response = await axios.get("http://localhost:8080/api/cars");
+        return response.data._embedded.cars;
+    }
+    
+    const {data, error, isSuccess} = useQuery({
+        queryKey: ["cars"],
+        queryFn: getCars
+    });
+
+    if (!isSuccess) {
+        return <span>Loading...</span>
+    } else if (error) {
+        return <span>Error when fetching data....</span>
+    } else {
+        return (
+            <table>
+                <tbody>
+                    {
+                        data.map((car: CarResponse) =>
+                            <tr key={car._links.self.href}>
+                                <td>{car.brand}</td>
+                                <td>{car.model}</td>
+                                <td>{car.color}</td>
+                                <td>{car.registrationNumber}</td>
+                                <td>{car.modelYear}</td>
+                                <td>{car.price}</td>
+                                <td></td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        )
+
+    }
+}
