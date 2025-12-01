4099cd5216b92518f7747254f230fb5335e64f72
zekariasm <zekariasmulu@gmail.com>
2025-12-01 00:02:47 -0800

    Adding test cases for carlist component

--- src/components/Carlist.tsx
+++ src/components/Carlist.tsx
@@ -1,41 +1,41 @@
-import { useQuery, useMutation, useQueryClient} from "@tanstack/react-query";
-import { deleteCar, getCars } from "../api/carapi";
-import type { GridCellParams, GridColDef } from '@mui/x-data-grid';
-import  { DataGrid } from '@mui/x-data-grid';
-import Snackbar from "@mui/material/Snackbar";
 import { useState } from "react";
-import AddCar from "./AddCar";
-import EditCar from "./EditCar";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { DataGrid } from '@mui/x-data-grid';
+import type { GridCellParams, GridColDef } from '@mui/x-data-grid';
+import Snackbar from "@mui/material/Snackbar";
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { deleteCar, getCars } from "../api/carapi";
+import AddCar from "./AddCar";
+import EditCar from "./EditCar";
 
 export default function Carlist() {
     const [open, setOpen] = useState(false);
     const queryClient = useQueryClient();
-
-    const {data, isError, isLoading, isSuccess} = useQuery({
+    
+    const { data, isError, isLoading, isSuccess } = useQuery({
         queryKey: ["cars"],
         queryFn: getCars
     });
-
+    
     const { mutate } = useMutation({
         mutationFn: deleteCar,
         onSuccess: () => {
             setOpen(true);
-            queryClient.invalidateQueries({queryKey: ['cars']})
+            queryClient.invalidateQueries({ queryKey: ['cars'] })
         },
         onError: (err: Error) => {
             console.error(err);
         }
     })
-
+    
     const columns: GridColDef[] = [
-        {field: 'brand', headerName: 'Brand', width: 125},
-        {field: 'model', headerName: 'Model', width: 125},
-        {field: 'color', headerName: 'Color', width: 125},
-        {field: 'registrationNumber', headerName: 'Reg.nr.', width: 125},
-        {field: 'modelYear', headerName: 'Model Year', width: 125},
-        {field: 'price', headerName: 'Price', width: 125},
+        { field: 'brand', headerName: 'Brand', width: 125 },
+        { field: 'model', headerName: 'Model', width: 125 },
+        { field: 'color', headerName: 'Color', width: 125 },
+        { field: 'registrationNumber', headerName: 'Reg.nr.', width: 125 },
+        { field: 'modelYear', headerName: 'Model Year', width: 125 },
+        { field: 'price', headerName: 'Price', width: 125 },
         {
             field: 'edit',
             headerName: '',
@@ -43,8 +43,8 @@ export default function Carlist() {
             sortable: false,
             filterable: false,
             disableColumnMenu: true,
-            renderCell: (params: GridCellParams) => 
-                <EditCar cardata={params.row}/>
+            renderCell: (params: GridCellParams) =>
+                <EditCar cardata={params.row} />
         },
         {
             field: 'delete',
@@ -54,47 +54,45 @@ export default function Carlist() {
             filterable: false,
             disableColumnMenu: true,
             renderCell: (params: GridCellParams) => (
-                <IconButton aria-label="delete" size="small"
-                onClick={() => {
-                    if (window.confirm(`Are you sure you want to delete ${params.row.brand} ${params.row.model}?`)) {
-                        mutate(params.row._links.car.href);
-                    }
-                }}>
-                   <DeleteIcon fontSize="small" />
+                <IconButton
+                    aria-label="delete"
+                    size="small"
+                    onClick={() => {
+                        if (window.confirm`Are you sure you want to delete ${params.row.brand} ${params.row.model}?`) {
+                            mutate(params.row._links.car.href);
+                        }
+                    }}
+                >
+                    <DeleteIcon fontSize="small" />
                 </IconButton>
             )
         }
-  
     ]
-
     
-  if (isLoading) {
-    return <span>Loading...</span>
-  }
-  else if (isError) {
-    return <span>Error when fetching cars...</span>
-  }
-  else if (isSuccess) {
-    return ( 
+    if (isLoading) {
+        return <span>Loading...</span>
+    }
+    else if (isError) {
+        return <span>Error when fetching cars...</span>
+    }
+    else if (isSuccess) {
+        return (
             <>
-            <AddCar />
-            <DataGrid
-            rows={data}
-            columns={columns}
-            disableRowSelectionOnClick={true}
-            getRowId={row => row._links.self.href}
-            showToolbar
-            />
-
-            <Snackbar
-            open={open}
-            autoHideDuration={2000}
-            onClose={() => setOpen(false)}
-            message="Car deleted"
-            />
+                <AddCar />
+                <DataGrid
+                    rows={data}
+                    columns={columns}
+                    disableRowSelectionOnClick={true}
+                    getRowId={row => row._links.self.href}
+                    showToolbar
+                />
+                <Snackbar
+                    open={open}
+                    autoHideDuration={2000}
+                    onClose={() => setOpen(false)}
+                    message="Car deleted"
+                />
             </>
-         
         )
-
     }
 }
